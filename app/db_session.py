c04660fac22725cb1a75dfc468b2094242cf45e9
Hyeongju Lee <zoocasso1590@gmail.com>
2023-12-07 19:04:26 +0900

    update

--- app/db_session.py
+++ app/db_session.py
@@ -23,25 +23,32 @@ db = SessionLocal()
 metadata = MetaData()
 user_tb = Table("user_tb", metadata, autoload_with=engine)
 music_tb = Table("music_tb", metadata, autoload_with=engine)
-test_tb = Table("test_tb", metadata, autoload_with=engine)
+playlog_tb = Table("playlog_tb", metadata, autoload_with=engine)
 
-def insert_test_tb(test_form):
-    test_tb_sql = insert(test_tb).values(test=f'{test_form.test}')
-    db.execute(test_tb_sql)
-
-def insert_music_tb(music_form):
-    insert_music_tb_sql = insert(music_tb).values(name=f'{music_form.name}',key_gubun=f'{music_form.key_gubun}',difficulty=f'{music_form.difficulty}',artist=f'{music_form.artist}',category=f'{music_form.category}')
+def insert_music_tb(insert_music_form):
+    insert_music_tb_sql = insert(music_tb).values(name=f'{insert_music_form.name}',key_gubun=f'{insert_music_form.key_gubun}',difficulty=f'{insert_music_form.difficulty}',artist=f'{insert_music_form.artist}',category=f'{insert_music_form.category}')
     db.execute(insert_music_tb_sql)
 
-def insert_user_tb(signup_form):
+def insert_user_tb(insert_user_form):
     insert_time = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-    insert_user_tb_sql = insert(user_tb).values(user_key=f'{signup_form.user_key}', device_id=f'{signup_form.device_id}', insert_date =f'{insert_time}')
+    insert_user_tb_sql = insert(user_tb).values(device_id=f'{insert_user_form.device_id}', insert_date =f'{insert_time}')
     db.execute(insert_user_tb_sql)
 
-def select_user_tb(select_form):
-    select_user_tb_sql = select(user_tb).where(user_tb.name == f'{select_form.user_key}')
+def select_user_tb(select_user_form):
+    select_user_tb_sql = select(user_tb).where(user_tb.c.device_id == select_user_form.device_id)
     result = db.execute(select_user_tb_sql)
+    return result
 
+def insert_playlog_tb(insert_playlog_form):
+    insert_time = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    insert_playlog_tb_sql = insert(playlog_tb).values(music_key=f'{insert_playlog_form.music_key}', user_key=f'{insert_playlog_form.user_key}', total_score=f'{insert_playlog_form.total_score}', insert_date =f'{insert_time}')
+    db.execute(insert_playlog_tb_sql)
+
+def select_playlog_tb(select_playlog_form):
+    select_playlog_tb_sql = select(playlog_tb).where(playlog_tb.c.user_key == select_playlog_form.user_key).where(playlog_tb.c.music_key == select_playlog_form.music_key)
+    result = db.execute(select_playlog_tb_sql)
+    return result
+    
 def db_commit_and_close():
     db.commit()
-    engine.close()
+    # engine.close()
--- app/main.py
+++ app/main.py
@@ -12,15 +12,7 @@ from fastapi import FastAPI, Request, HTTPException, status
 ## fastapi 인스턴스 저장
 app = FastAPI()
 
-class TestForm(BaseModel):
-    test : str
-
-@app.post("/insert_test")
-async def insert_test(request:Request, test_form:TestForm):
-    db_session.insert_test_tb(test_form)
-    return 1
-
-class MusicForm(BaseModel):
+class InsertMusicForm(BaseModel):
     name : str
     key_gubun : int
     difficulty : int
@@ -28,30 +20,54 @@ class MusicForm(BaseModel):
     category : str
 
 @app.post("/insert_music")
-async def insert_music(request:Request, music_form:MusicForm):
-    db_session.insert_music_tb(music_form)
+async def insert_music(request:Request, insert_music_form:InsertMusicForm):
+    db_session.insert_music_tb(insert_music_form)
     return 1
 
-class SignupForm(BaseModel):
-    user_key : str
+class InsertUserForm(BaseModel):
     device_id : str
 
-@app.post("/signup")
-async def signup(request:Request, signup_form:SignupForm):
-    db_session.insert_user_tb(signup_form)
-    return 1
+@app.post("/insert_user")
+async def insert_user(request:Request, insert_user_form:InsertUserForm):
+    db_session.insert_user_tb(insert_user_form)
+    db_session.db_commit_and_close()
 
-class Select_form(BaseModel):
-    user_key : str
+class InsertPlaylogForm(BaseModel):
+    music_key: str
+    user_key: str
+    total_score: str
+
+@app.post("/insert_playlog")
+async def insert_playlog(request:Request, insert_playlog_form:InsertPlaylogForm):
+    db_session.insert_playlog_tb(insert_playlog_form)
+    db_session.db_commit_and_close()
+
+class SelectUserForm(BaseModel):
+    device_id : str
 
 @app.post("/select_user")
-async def main(request:Request, select_form:Select_form):
-    db_session.select_user_tb(select_form)
-    return "complete Selct data"
-
-@app.post("/db_commit_and_close")
-async def db_commit_and_close():
+async def select_user(request:Request, select_user_form:SelectUserForm):
+    user_info = db_session.select_user_tb(select_user_form)
     db_session.db_commit_and_close()
+
+    result_row = user_info.fetchall()
+    return str(result_row)
+
+class SelectLogForm(BaseModel):
+    user_key : str
+    music_key : str
+
+@app.post("/select_playlog")
+async def select_playlog(request:Request, select_playlog_form:SelectLogForm):
+    playlog_info = db_session.select_playlog_tb(select_playlog_form)
+    db_session.db_commit_and_close()
+
+    result_row = playlog_info.fetchall()
+    return str(result_row)
+
+# @app.post("/db_commit_and_close")
+# async def db_commit_and_close():
+#     db_session.db_commit_and_close()
 
 @app.get("/")
 async def main(request:Request):
